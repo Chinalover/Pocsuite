61e8009dff14c98b79cb97992d1d926868089558
Wyc0 <wuyccc@gmail.com>
2015-10-25 17:01:00 +0800

    enhance missile

--- pocsuite/missile.py
+++ pocsuite/missile.py
@@ -2,10 +2,12 @@
 # -*- coding:utf-8 -*-
 
 """
-Copyright
+Copyright (c) 2014-2015 pocsuite developers (http://sebug.net)
+See the file 'docs/COPYING' for copying permission
 """
 
-import sys
+# import sys; sys.path.append('Pocsuite/pocsuite') 将pocsuite.py路径添加到sys.path
+# 运行时需要在调用Missile的目录创建modules/tmp/文件夹
 from pocsuite import pcsInit
 from lib.core.settings import PCS_OPTIONS
 from lib.core.common import banner
@@ -15,17 +17,17 @@ class Missile():
     def __init__(self, target, missile_info={}):
 
         if missile_info['db']:
-            import known
-            missile_info[ids] = read_db(id)
-        else:
-            missile_info[ids] = id2path()
-
+            # from knwonledge import read_db
+            with open('modules/ssv_%s.py' % missile_info['ids'], 'a') \
+                    as fp:
+                fp.write(self.read_db(id))
+            missile_info['ids'] = 'modules/ssv_%s.py' % missile_info['ids']
 
         PCS_OPTIONS.update({
             'url': target,
             'pocFile': missile_info['ids'],
-            'headers': headers,
-            'extra_params': params,
+            'headers': missile_info['headers'],
+            'extra_params': missile_info['params'],
             'Mode': missile_info['mode']
         })
 
@@ -33,12 +35,23 @@ class Missile():
         result = pcsInit(PCS_OPTIONS)
         return result[0]
 
+    def id2path():
+        pass
+
+    def read_db(self, t):
+        with open('Pocsuite/pocsuite/tests/poc_example1.py') as fp:
+            content = fp.read()
+        return content
+
+
 
 if __name__ == '__main__':
     missile_info = {
         'mode': 'verify',  # 默认为verify，可以不包含该字段
-        'ids': 'tests/poc_example1.py',  # 默认为空，可以不包含该字段
-        'db': 
+        'ids': '123',  # 默认为空，可以不包含该字段
+        'headers': None,
+        'params': None,
+        'db':  '123'
     }
     target = 'baidu.com'
 
@@ -46,3 +59,6 @@ if __name__ == '__main__':
     res = mis.run()
     print res
     print res[-1].result
+"""
+import sys; sys.path.append('Pocsuite/pocsuite');from missile import Missile
+"""
